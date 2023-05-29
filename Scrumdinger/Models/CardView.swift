6305ac31737b0cf3909f7d87ab7d8434d2be34fe
Guangyao Quan <2522089516quan@gmail.com>
2023-05-29 22:18:29 +0200

    Sync local repo in Xcode with GitHub

--- Scrumdinger/Models/CardView.swift
+++ Scrumdinger/Models/CardView.swift
@@ -5,4 +5,33 @@
 //  Created by Guangyao Quan on 29.05.23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+struct CardView_Preview: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
